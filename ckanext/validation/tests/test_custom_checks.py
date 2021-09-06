65ba85efd3d3fc4cb3914e959b9a2f40d5111191
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-09-06 12:29:42 +0100

    ADX-635 Test ascii encoding

--- ckanext/validation/tests/test_custom_checks.py
+++ ckanext/validation/tests/test_custom_checks.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from goodtables import validate
 from ckanext.validation.custom_checks import setup_custom_goodtables
-
+from ckanext.validation.jobs import run_validation_job
+from ckanext.validation.model import Validation
+from ckan.tests import factories
+import pandas
+from StringIO import StringIO
 setup_custom_goodtables()
 
 
@@ -51,3 +55,62 @@ def test_check_custom_constraint_incorrect_constraint(log):
         (1, 2, None, 'custom-constraint'),
         (1, 2, None, 'custom-constraint'),
     ]
+
+
+def test_check_custom_constraint_non_ascii_characters(log):
+    source = [
+        ['row', 'name', 'bonus'],
+        [2, u'Aléx', '200'],
+    ]
+    report = validate(source, checks=[
+        {'custom-constraint': {'constraint': 'bonus > 100'}}
+    ])
+    assert report['valid'] == True
+
+
+# Validate
+def test_enumerable_with_non_ascii_characters(log):
+    source = [
+        ['area_name', 'number'],
+        [u'Ségou', 200],
+        [u'Mopti', 500]
+    ]
+
+    schema = {
+        "fields": [{
+            "name": "area_name",
+            "type": "string",
+            "constraints": {
+                "enum": [u'Ségou', u'Mopti']
+            }
+        }, {
+            "name": "number",
+            "type": "number",
+            "constraints": {
+                "enum": [200, 500]
+            }
+        }]
+    }
+    report = validate(source, schema=schema)
+    assert report['valid'] is True
+
+
+# Validate
+def test_unique_with_non_ascii_characters(log):
+    source = [
+        ['area_name', 'number'],
+        [u'Ségou', 200],
+        [u'Mopti', 500]
+    ]
+    schema = {
+        "fields": [{
+            "name": "area_name",
+            "type": "string"
+        }, {
+            "name": "number",
+            "type": "number"
+        }],
+        "primaryKey": ["area_name", "number"]
+    }
+    report = validate(source, schema=schema)
+    assert report['valid'] is True
