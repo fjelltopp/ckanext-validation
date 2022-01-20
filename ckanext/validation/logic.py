fjelltopp/ckanext-validation

commit e4ef44962202a225ca9e3aefa09f7e3c3b367f92

Revert "ADX-758 Only batch-validate resources in package with schema field."

ckanext/validation/logic.py
@@ -310,8 +310,6 @@ def resource_validation_run_batch(context, data_dict):
                     if (not resource.get(u'format', u'').lower()
                             in settings.SUPPORTED_FORMATS):
                         continue
-                    elif not resource.get(u'schema'):
-                        continue
 
                     try:
                         t.get_action(u'resource_validation_run')(

commit a0d0681a094b509eb542c18a33a21372a5d91d7a

Fix confusion caused by e4ef449622 ensuring there is nothing in master that is not in development.
This reverts commit e4ef44962202a225ca9e3aefa09f7e3c3b367f92.

ckanext/validation/logic.py
@@ -310,6 +310,8 @@ def resource_validation_run_batch(context, data_dict):
                     if (not resource.get(u'format', u'').lower()
                             in settings.SUPPORTED_FORMATS):
                         continue
+                    elif not resource.get(u'schema'):
+                        continue
 
                     try:
                         t.get_action(u'resource_validation_run')(