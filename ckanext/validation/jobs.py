2ff53cc51aa374bc294e994db229b383b7d9f21f
Chas Nelson <github@chasnelson.co.uk>
2023-07-31 12:05:03 +0100

    delete me wip

--- ckanext/validation/jobs.py
+++ ckanext/validation/jobs.py
@@ -254,7 +254,8 @@ def _prepare_foreign_keys(dataset, schema):
                     {'foreignKey': 'Foreign key reference does not exist. ' +
                     'Must be a url, json object or a resource in this dataset but was: ' +
                     foreign_key['reference']['resource'] +
-                    ' Available resources: ' + str(dataset_resources.keys())})
+                    ' Available resources: ' + str(dataset_resources.keys()) +
+                    schema})
         
         referenced_resources.append({'name': foreign_key['reference']['resource'], 'path': foreign_key_resource, 'format': foreign_key_format})
 
